c1c7e5c7c6bb4ba75e51cece188aa6b0f4d5507b
alejandroToledo <alejandrotol.her@gmail.com>
2020-09-21 18:26:52 -0300

    usuario terminado

--- web/frontend/src/pages/LogIn.js
+++ web/frontend/src/pages/LogIn.js
@@ -11,46 +11,47 @@ const LogIn = props => {
     const [ingresoUsuario, setIngresoUsuario] = useState({
         mail: '',
         pass: ''
-     })
+    })
 
-     const leerImput = e => {
+    const leerImput = e => {
         const campo = e.target.name
         const valor = e.target.value
-        setIngresoUsuario ({
+        setIngresoUsuario({
             ...ingresoUsuario,
             [campo]: valor
         })
     }
 
-    const enviarInfo  = async e => {
+    const enviarInfo = async e => {
         e.preventDefault()
         await props.logUser(ingresoUsuario)
     }
 
-   // useEffect(()=>{
-  //      if(props.success){
-  //          alert("Welcome")
-  //         props.history.push("/home")  
-  //      }
-  //  },[props.success]) 
+    // useEffect(()=>{
+    //      if(props.success){
+    //          alert("Welcome")
+    //         props.history.push("/home")  
+    //      }
+    //  },[props.success]) 
 
     const responseGoogle = respuesta => {
-        props.loguearUsuario({
-            mail:respuesta.profileObj.email,
-            pass:respuesta.profileObj.googleId})
-        props.history.push("/home")  
+        props.logUser({
+            mail: respuesta.profileObj.email,
+            pass: respuesta.profileObj.googleId
+        })
+        props.history.push("/home")
     }
 
-    
 
-        return (   
-            <div id="todoelhome">   
 
-              <div id="divFormulario">
-                  <form>
-                     <h3>Log In form</h3>
+    return (
+        <div id="todoelhome">
 
-                     <GoogleLogin id="GoogleLogin"
+            <div id="divFormulario">
+                <form>
+                    <h3>Log In form</h3>
+
+                    <GoogleLogin id="GoogleLogin"
                         clientId="83311303903-d8eviki4j9rvmabuc0ceg0c5im70b2ab.apps.googleusercontent.com"
                         buttonText="Crear cuenta con google"
                         onSuccess={responseGoogle}
@@ -58,25 +59,25 @@ const LogIn = props => {
                         cookiePolicy={'single_host_origin'}
                     />
 
-                     <div id="divUsuario">
-                         <input onChange={leerImput} type="text" id="mail" name="mail" placeholder="Write your mail here"></input>
-                     </div>
-                     <div id="divContraseña">
-                         <input onChange={leerImput} type="password" id="pass" name="pass" placeholder="Write your password here"></input>
-                     </div>
-                     <button onClick={enviarInfo}>Log in</button>
-                  </form>
-              </div>
-              
-          </div>
-        )
-    
+                    <div id="divUsuario">
+                        <input onChange={leerImput} type="text" id="mail" name="mail" placeholder="Write your mail here"></input>
+                    </div>
+                    <div id="divContraseña">
+                        <input onChange={leerImput} type="password" id="pass" name="pass" placeholder="Write your password here"></input>
+                    </div>
+                    <button onClick={enviarInfo}>Log in</button>
+                </form>
+            </div>
+
+        </div>
+    )
+
 }
 
 
 
 const mapDispatchToProps = {
     logUser: authActions.logUser,
- }
+}
 
-export default connect(null, mapDispatchToProps) (LogIn)
+export default connect(null, mapDispatchToProps)(LogIn)
