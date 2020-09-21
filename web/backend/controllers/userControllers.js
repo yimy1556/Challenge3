9c5385fa81c80560aab6c8155fe79289c1561e1e
Yimy LLanos <llanosyimy@gmail.com>
2020-09-21 16:56:00 -0300

    passport y controllers terminado y rutas

--- web/backend/controllers/userControllers.js
+++ web/backend/controllers/userControllers.js
@@ -1,36 +1,56 @@
-mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+const User = require("../models/User")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const message = "Mail or Password incorrect"
 
-const userSchema = new mongoose.Schema({
-	firstName: {
-		type: String,
-		trim: true
+const userController = {
+	createUser: async (req, res) => {
+		const { pass } = req.body
+		const newUser = new User({ ...req.body })
+		newUser.pass = bcrypt.hashSync(pass.trim(), 10)
+		console.log(newUser)
+		newUser.save()
+		.then(user => {
+			const token = jwt.sign({ ...user }, process.env.SECRET_KEY, {})
+			if (!token)
+				return res.json({
+					success: false,
+					error: "Error al crear el usuario",
+				})
+			res.json({
+				success: true,
+				token,
+				username: user.username,
+				urlPic: user.urlPic,
+				likes: user.likes,
+			})
+		})
+		.catch(err => res.json({ success: "false", error: err }))
 	},
-	lastName: {
-		type: String,
-		trim: true
-	},
-	mail: {
-		type: String,
-		required: true,
-        trim: true,
-        unique: true
-    },
-	pass: {
-		type: String,
-		required: true
-	},
-	urlPic: {
-		type: String,
-		default: false,
-		trim: true
-	},
-	direction: {
-		type: Array,
-		default: []
-	}
-})
+	loginUser: async (req, res) => {
+		const { mail, pass } = req.body
 
-const User = mongoose.model('user', userSchema);
-userSchema.plugin(uniqueValidator,{ message: 'email esta en uso'});
-module.exports = User;
+		const userExists = await User.findOne({ mail })
+		if (!userExists) return res.json({ success: false, error: message })
+		const passwordMatches = bcrypt.compareSync(pass, userExists.pass)
+
+        if (!passwordMatches) return res.json({ success: false, error: message })
+		const token = jwt.sign({ ...userExists }, process.env.SECRET_KEY, {})
+		if (!token) return res.json({ success: false, error })
+
+		res.json({
+			success: true,
+			token,
+			username: userExists.username,
+			urlPic: userExists.urlPic,
+			likes: userExists.likes,
+			id: userExists._id,
+		})
+	},
+	getUser: (req, res) => {
+		const { urlPic, username, likes } = req.user
+		res.json({urlPic, username, likes,})
+    }
+}
+
+module.exports = userController
