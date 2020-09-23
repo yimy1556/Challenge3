46ddd8f2dccd407380b445d6423951a94ae4bf52
Luciana Mendez <lucianamendez95@hotmail.com>
2020-09-23 18:55:33 -0300

    foto

--- web/frontend/src/pages/Product.js
+++ web/frontend/src/pages/Product.js
@@ -6,13 +6,13 @@ import '../styles/shop.css'
 class Product extends React.Component {
 
     render() {
-        console.log(this.props)
-        return (
+        console.log(this.props.product)    
+            return (
             <>
                 <NavLink to={`./selectProduct/${this.props.product._id}`} className="linkProduct">
                     <div id="articulo">
                         <h3>{this.props.product.title}</h3>
-                        <div id="imagenShop" style={{ backgroundImage: `url(${this.props.product.photo})`, width: '20vw', height: '20vh' }}></div>
+                        <div id="imagenShop" style={{ backgroundImage: `url(${this.props.product.variants[0].photo})`, width: '20vw', height: '20vh' }}></div>
                         <p id="descripcionShop">{this.props.product.description}</p>
                         <p id="precioShop">{this.props.product.price}$</p>
                     </div>
