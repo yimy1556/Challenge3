7b424585b765f263b056f27df932cd40f20b2dfe
Luciana Mendez <lucianamendez95@hotmail.com>
2020-09-25 18:58:01 -0300

    shop estilos

--- web/frontend/src/pages/Product.js
+++ web/frontend/src/pages/Product.js
@@ -39,18 +39,17 @@ const Product = (props) => {
     return (
         <>
             <div id="articulo">
-                <h3>{props.product.title}</h3>
                 <div id="imagenShop" style={{ backgroundImage: `url(${foto.color})` }}> </div>
                 <div id="fotosChicas">
                     {variantsAux.map(variant => {
-                        return (<div id="imagenShopChica" onClick={e => cambiarFoto(variant.photo)} style={{ backgroundImage: `url(${variant.photo})`, width: '8vh', height: '8vh' }}> </div>)
+                        return (<div id="imagenShopChica" onClick={e => cambiarFoto(variant.photo)} style={{ backgroundColor: `${variant.color}`}}> </div>)
                     })}
                 </div>
                 <p id="descripcionShop">{props.product.description}</p>
                 <p id="precioShop">{props.product.price}$</p>
                 <NavLink to={`./selectProduct/${props.product._id}`} className="linkProduct">
                     <div id="divBotonVerMas">
-                        <button className="createAccount">View more</button>
+                        <button className="createAccount"  style={{fontSize:"3vh"}}>View more</button>
                     </div>
                 </NavLink>
             </div>
