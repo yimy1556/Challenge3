88d0ead1f6316b577734cff78b860de3df3ebc52
Luciana Mendez <lucianamendez95@hotmail.com>
2020-09-29 15:47:17 -0300

    hola

--- mobile/pyral/App.js
+++ mobile/pyral/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import Header from './App/Components/Header'
-import Home from './App/Screens/Home'
 
 import Tab from './App/Nagivator/TabV'
 export default function App() {
     return (<>
         <Header/> 
-        <Home/>
         <Tab/>
     </>);
 }
