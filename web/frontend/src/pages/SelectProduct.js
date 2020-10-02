185b365fbfa22f6bf40d28adc762b9ae7d9d3d58
alejandroToledo <alejandrotol.her@gmail.com>
2020-10-02 19:26:56 -0300

    estilos

--- web/frontend/src/pages/SelectProduct.js
+++ web/frontend/src/pages/SelectProduct.js
@@ -225,27 +225,6 @@ const SelectProduct = (props) => {
                 }
             </div>
         </div>
-        <div style={{ width: "540px", marginLeft: "20px", overflow: "hidden" }}>
-            <ImageZoom
-                portalId="portal"
-                largeImgSrc={largeCatImg}
-                imageWidth={540}
-                imageHeight={540}
-                zoomContainerWidth={540}
-                activeClass="my-active"
-                portalStyle={Object.assign(
-                    { ...ImageZoom.defaultPortalStyle },
-                    { top: "140px" }
-                )}
-                zoomScale={1}
-                responsive={true}
-            >
-                <img src={largeCatImg} alt="Cat image" width="100%" />
-            </ImageZoom>
-        </div>
-
-        <div id="portal" className="slick-side" />
-
     </>
     )
 }
