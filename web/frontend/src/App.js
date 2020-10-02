19e98fe3562992557ee1f855e590671958297868
alejandroToledo <alejandrotol.her@gmail.com>
2020-10-02 11:44:13 -0300

    cambios rutas

--- web/frontend/src/App.js
+++ web/frontend/src/App.js
@@ -54,6 +54,7 @@ function App(props) {
         <Route path="/selectProduct/:id" component={SelectProduct} />
         <Route path="/profile" component={Profile} />
         <Route path="/lowNewsletter" component={LowNewsletter} />
+        <Route path="/buy" component={Buy} />
         <Route path="/address" component={Address} />
         <Route path="/faqs" component={Faqs} />
         <Redirect to="/" />
@@ -70,6 +71,7 @@ function App(props) {
       <Route exact path="/shop" component={Shop} />
       <Route path="/selectProduct/:id" component={SelectProduct} />
       <Route path="/lowNewsletter" component={LowNewsletter} />
+      <Route path="/buy" component={Buy} />
       <Route path="/faqs" component={Faqs} />
       <Redirect to="/" />
     </Switch>
