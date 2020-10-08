e52e548838cb2497d73b0d882fe065d0532af45d
paulinavaira <paulinavaira@users.noreply.github.com>
2020-10-08 09:38:48 -0300

    styles pt.6

--- web/frontend/src/pages/Buy.js
+++ web/frontend/src/pages/Buy.js
@@ -24,8 +24,8 @@ const Buy = (props) => {
     console.log(props.listProduct);
     return (
         <>
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', backgroundImage: `url(https://cdn-yotpo-images-production.yotpo.com/instagram/85/17967839827327785/standard_resolution.jpg)`, width: '100%', height: '30vh', backgroundPosition: 'center 45%', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
-                <h2 style={{ color: 'white', textAlign: 'center', fontSize: 'bold' }}>Payment</h2>
+            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignContent: 'center', backgroundImage: `url(https://cdn-yotpo-images-production.yotpo.com/instagram/85/17967839827327785/standard_resolution.jpg)`, width: '100%', height: '40vh', backgroundPosition: 'center 45%', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }}>
+                <h1 style={{ color: 'white', textAlign: 'center', fontSize: 'bold' }}>PAYMENT</h1>
             </div>
             <div style={{ display: 'flex', justifyContent: 'center',  width:'80%', margin:'2vh auto'}}>
                     <div style={{ borderRight: '1px solid #DFDFDF', padding:'0vh 7vh', margin:'0vh 7vh', width:'60%'}}>
@@ -34,8 +34,6 @@ const Buy = (props) => {
                             <div style={{ display: 'flex', flexDirection:'column'}}>
                                 <p style={{margin:'2vh 0vh' }}><strong>Information</strong> {'>'} Payment</p>
                                 <h3>Contact information<NavLink to="address" style={{ color: 'grey', paddingLeft: '2em' }}><CreateIcon /></NavLink></h3>
-
-
                             </div>
                             <div  style={{fontWeight: 'lighter',  borderRadius: '7px', border:'1px solid #DFDFDF', width:'100%', padding:'2vh', margin:'2vh 0vh'}}>
                                 {props.userlogged.contact.map((x) => {
@@ -70,7 +68,7 @@ const Buy = (props) => {
                             {props.listProduct.map(prod => <div style={{ display: 'flex', width: '100%',  margin:'1vh 0 '}}>
                                 <div style={{ border:'1px solid #DFDFDF', borderRadius:'15%'}}><div style={{backgroundImage:`url(${prod.remeraActual})`, height:'10vh', width:'10vh',backgroundPosition: 'center', backgroundSize: 'contain', backgroundRepeat: 'no-repeat'}}></div></div>
                                 <div style={{display:'flex', flexDirection:'column', alignSelf:'center'}}>
-                                    <div style={{display:'flex', margin:'0vh 1vh ', width:'100%', justifyContent:'space-between'}}>
+                                    <div style={{display:'flex', margin:'0vh 1vh ', justifyContent:'space-between'}}>
                                         <p style={{fontWeight:'bold'}}>{prod.color} {prod.title} ({prod.cant})</p>
                                         <p style={{fontWeight:'bold'}}>${prod.price * prod.cant}</p>
                                     </div>
